4865d12dfd98d6f4c5f26cca7f73b63599f13132
mattplm <hello@mattplm.fr>
2023-11-21 22:49:03 +0100

    Fixup download script

--- download.py
+++ download.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+import datetime
 
 import urllib.request
+urllib.request.HTTPHandler(debuglevel=1)
 
 
 if __name__ == '__main__':
-    for year in range(2020, 2021):
+    today = datetime.date.today
+    max_year = today.year
+    for year in range(2015, max_year):
         for i in range(1, 26):
             opener = urllib.request.build_opener()
             with open('session-cookie.txt') as f:
                 for line in f:
                     opener.addheaders = [('Cookie', line.strip())]
             urllib.request.install_opener(opener)
-            open(str(year) + '/inputs/' + str(i), 'w')
-            url = 'https://adventofcode.com/' + str(year) + '/day/' + str(i) + '/input'
-            urllib.request.urlretrieve(url, str(year) + '/inputs/' + str(i))
+            open(f'{year}/inputs/{i}', 'w')
+            url = 'https://adventofcode.com/'
+            urllib.request.urlretrieve(url, f'{year}/inputs/{i}')
